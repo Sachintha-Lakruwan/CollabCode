40f9bcd938aa31303ed359378088e27768c2901a
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2025-08-15 02:13:38 +0530

    implement static ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,46 @@
 import { NextUIProvider } from "@nextui-org/react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
-  return <NextUIProvider>{/* your app components */}</NextUIProvider>;
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const [code, setCode] = useState<string>("");
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+      inputRef.current.select();
+    }
+  }, []);
+  return (
+    <NextUIProvider>
+      <div className=" w-full h-[100vh] p-6">
+        <div className="flex justify-between">
+          <div className=" font-bold text-3xl">CollabCode</div>
+          <div className=" flex gap-3">
+            <ul className=" flex gap-2">
+              {[1, 2].map((item, index) => (
+                <li
+                  className="h-10 aspect-square bg-zinc-200 rounded-full"
+                  key={index}
+                ></li>
+              ))}
+            </ul>
+            <button className=" bg-green-600 text-white font-bold px-6 py-2 rounded-sm">
+              Run
+            </button>
+          </div>
+        </div>
+        <div className=" w-full h-[calc(100vh-7rem)] p-6 rounded-2xl mt-4 bg-zinc-50 outline-1">
+          <textarea
+            ref={inputRef}
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+            className="w-full font-mono h-full resize-none outline-none border-none"
+            placeholder="Type here..."
+          ></textarea>
+        </div>
+      </div>
+    </NextUIProvider>
+  );
 }
 
 export default App;
